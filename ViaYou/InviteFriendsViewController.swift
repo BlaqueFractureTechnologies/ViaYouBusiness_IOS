b988bc340a3dc1536459c9129008525ebf00dbe3
Promptchu <development@promptchu.com>
2019-09-26 12:41:20 +1000

    Signin ViewController UI changes

---
+++ ViaYou/InviteFriendsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InviteFriendsViewController.swift
+//  ViaYou
+//
+//  Created by Promptchu Pty Ltd on 26/9/19.
+//  Copyright © 2019 Promptchu Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class InviteFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
