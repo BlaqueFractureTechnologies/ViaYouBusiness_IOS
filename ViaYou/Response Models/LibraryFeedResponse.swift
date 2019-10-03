093f20e690fbf400aebd46255971d547485a73dd
Promptchu <development@promptchu.com>
2019-10-03 12:43:11 +1000

    minor ui fixes

---
+++ ViaYou/Response Models/LibraryFeedResponse.swift
@@ -0,0 +1,10 @@
+//
+//  LibraryFeedResponse.swift
+//  ViaYou
+//
+//  Created by Promptchu Pty Ltd on 3/10/19.
+//  Copyright © 2019 Promptchu Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
