9948436971704cb3a3ead30650d69039bd56f9bf
Promptchu <development@promptchu.com>
2019-10-08 09:46:50 +1100

    Search implementation in Contacts page

--- ViaYou/ContactsViewController.swift
+++ ViaYou/ContactsViewController.swift
@@ -11,16 +11,20 @@ import MessageUI
 //import FacebookShare
 import FBSDKShareKit
 
-class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,MFMessageComposeViewControllerDelegate, UINavigationControllerDelegate, SharingDelegate {
+class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,MFMessageComposeViewControllerDelegate, UINavigationControllerDelegate, SharingDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchField: UITextField!
     
     var dataArray:[PhoneContact] = []
     var passedUrlLink: String = ""
+    var isSearch: Bool!
+    var sortedArray:[PhoneContact] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        isSearch = false
+        self.hideKeyboardWhenTappedAround()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,8 +44,50 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
         }
     }
     
+    //search
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var searchText  = textField.text! + string
+        
+        if string  == "" {
+            searchText = (searchText as String).substring(to: searchText.index(before: searchText.endIndex))
+        }
+        
+        if searchText == "" {
+            isSearch = false
+            tableView.reloadData()
+        }
+        else{
+            getSearchArrayContains(searchText)
+        }
+        
+        return true
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func getSearchArrayContains(_ text : String) {
+//        let predicate : NSPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", text)
+//        sortedArray = (dataArray as NSArray).filtered(using: predicate) as! [PhoneContact]
+//        isSearch = true
+//        tableView.reloadData()
+        
+        sortedArray = dataArray.filter({ data in
+            return data.fullName.contains(text)
+        })
+        isSearch = true
+        tableView.reloadData()
+    }
+    //search complete
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        if isSearch! {
+            return sortedArray.count
+        }
+        else{
+            return dataArray.count
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -50,10 +96,21 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsTableViewCell", for: indexPath) as! ContactsTableViewCell
-        cell.configureCell(dataDict: dataArray[indexPath.row])
+        if isSearch! {
+           // cell.textLabel?.text = sortedArray[indexPath.row]
+            cell.configureCell(dataDict: sortedArray[indexPath.row])
+            
+            cell.inviteButton.tag = indexPath.row
+            cell.inviteButton.addTarget(self, action: #selector(inviteButtonClicked), for: UIControl.Event.touchUpInside)
+        }
+        else{
+            
+            cell.configureCell(dataDict: dataArray[indexPath.row])
+            
+            cell.inviteButton.tag = indexPath.row
+            cell.inviteButton.addTarget(self, action: #selector(inviteButtonClicked), for: UIControl.Event.touchUpInside)
+        }
         
-        cell.inviteButton.tag = indexPath.row
-        cell.inviteButton.addTarget(self, action: #selector(inviteButtonClicked), for: UIControl.Event.touchUpInside)
         return cell
     }
     
